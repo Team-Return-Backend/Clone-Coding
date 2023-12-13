83555d9d84df0feff7844c7b236b12c1ca8996f0
ilyoil2 <0211ilyoil@gmail.com>
2023-12-13 20:11:46 +0900

    🧩:: JwtFilterConfig 추가

--- together-infrastructure/src/main/java/com/example/together/global/config/JwtFilterConfig.java
+++ together-infrastructure/src/main/java/com/example/together/global/config/JwtFilterConfig.java
@@ -1,4 +1,29 @@
 package com.example.together.global.config;
 
-public class JwtFilterConfig {
+import com.example.together.global.error.GlobalExceptionFilter;
+import com.example.together.global.security.jwt.JwtTokenFilter;
+import com.example.together.global.security.jwt.JwtParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtFilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final ObjectMapper objectMapper;
+    private final JwtParser jwtParser;
+
+    @Override
+    public void configure(HttpSecurity http) {
+
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtParser);
+        GlobalExceptionFilter globalExceptionFilter = new GlobalExceptionFilter(objectMapper);
+
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(globalExceptionFilter, JwtTokenFilter.class);
+    }
+
 }
